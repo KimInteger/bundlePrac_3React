23a7a6b851edc39af8018aca2564c8ba5f480656
KimInteger <kjs20151240@gmail.com>
2024-07-09 10:35:32 +0900

    add_김정수 : app.tsx파일 생성

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App : React.FC = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(<App/>)
