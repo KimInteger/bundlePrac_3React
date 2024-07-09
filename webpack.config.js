c1ab084460cf296c3119e8b47cb49df063985d67
KimInteger <kjs20151240@gmail.com>
2024-07-09 11:28:32 +0900

    chore_김정수 : entry변경 webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry : './src/app.tsx',
+  entry : './src/index.tsx',
   output : {
     path : path.resolve(__dirname,'dist'),
     filename : 'bundle.js'
