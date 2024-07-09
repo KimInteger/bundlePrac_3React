22da7cf42c1d5fc2a81e6d3b2ce0947cd483851e
KimInteger <kjs20151240@gmail.com>
2024-07-09 11:42:22 +0900

    add_김정수 : app.tsx 기본적인 react코드 및 함수호출

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Minam = () => {
+  return (
+    <h1>미남은 당연히 공욱재</h1>
+  );
+}
+
+const App : React.FC = () => {
+  return (
+    <div>
+      <Minam />
+      <h1>안녕?</h1>
+    </div>
+  )
+}
+
+export default App
