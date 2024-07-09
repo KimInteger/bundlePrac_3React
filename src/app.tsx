847f5870c07669d6a6edd244dc9f1add8e82acef
KimInteger <kjs20151240@gmail.com>
2024-07-09 12:03:26 +0900

    move_김정수 : Minam함수 새로운 파일로 이동

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import basicData from '../static/basic_data'
+import Minam from './Minam'
 
-
-interface textNode {
-  textNode : string;
-}
-
-const Minam : React.FC<textNode> = ({textNode}) => {
-  return (
-    <h1>{textNode}</h1>
-  );
-}
 
 const App : React.FC = () => {
   return (
